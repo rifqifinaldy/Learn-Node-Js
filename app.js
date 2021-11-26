cd7cbff9789af8b48f9d2b954e92169d922c425d
rifqifinaldy <rifqifinaldy@gmail.com>
2021-11-26 23:29:52 +0700

    Added | Redirect & 404 Page Features

--- app.js
+++ app.js
@@ -7,5 +7,21 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.send('<p>Home Page</p>');
+    // res.send('<p>Home Page</p>');
+    res.sendFile('./views/index.html', {root: __dirname});
+})
+
+app.get('/about', (req, res) => {
+    // res.send('<p>About Page</p>');
+    res.sendFile('./views/about.html', {root: __dirname});
+})
+
+// REDIRECTS
+app.get('/about-us', (req,res) => {
+    res.redirect('./about');
+})
+
+// 404 Page
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', {root: __dirname})
 })

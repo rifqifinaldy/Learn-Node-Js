8bf2974eab7613ab22e86a005c73faa3e068c934
rifqifinaldy <rifqifinaldy@gmail.com>
2021-11-30 21:22:18 +0700

    Basic | Engine View

--- app.js
+++ app.js
@@ -3,25 +3,37 @@ const express = require('express');
 // EXPRESS APP
 const app = express();
 
+// Register View Engine
+app.set('view engine', 'ejs');
+
 // Listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
     // res.send('<p>Home Page</p>');
-    res.sendFile('./views/index.html', {root: __dirname});
-})
+    const komentar = [
+        {title: 'Judul Komentar #1', author: 'Si A', comment: 'Ieu naon sih'},
+        {title: 'Judul Komentar #2', author: 'Si B', comment: 'Komen ah'},
+        {title: 'Judul Komentar #3', author: 'Si C', comment: 'Nyageus'}
+    ]
+    res.render('index', {title: 'HOME', komentar});
+});
 
-app.get('/about', (req, res) => {
+app.get('/komentar', (req, res) => {
     // res.send('<p>About Page</p>');
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('komentar-list', {title: 'Komentar'})
 })
 
 // REDIRECTS
-app.get('/about-us', (req,res) => {
-    res.redirect('./about');
+app.get('/komentar-anda', (req,res) => {
+    res.redirect('/komentar');
+})
+
+app.get('/komentar/add', (req,res) => {
+    res.render('komentar-tambah', {title: 'Tambah'});
 })
 
 // 404 Page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname})
+    res.status(404).render('404')
 })
